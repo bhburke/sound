3a30f379078754bfa364ccbcf25e4453ddd5ca27
Ben Burke <bhburke@umich.edu>
2012-11-02 23:19:01 -0400

    changing sm2

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <head>
 <title>Sound</title>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-<script src="js/soundmanager2-nodebug-jsmin.js"></script>
+<!--<script src="js/soundmanager2-nodebug-jsmin.js"></script>-->
+<script src="js/soundmanager2.js"></script>
 <script src="js/sound.js"></script>
 <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>

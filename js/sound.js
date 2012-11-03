aa31bc11d1cf45abb77c5d47414d43c7d7f06d6e
Ben Burke <bhburke@umich.edu>
2012-11-02 21:55:08 -0400

    click anim

--- js/sound.js
+++ js/sound.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
+	$("#button img").mousedown(function() {
+   	 	$(this).attr("src", "speaker_icon_inverted.png");
+		}).mouseup(function() {
+    	$(this).attr("src", "speaker_icon.png");
+});
+
+
 });
 
 function getNewSound(){
